271bd361948dd5fa6c0168ee051559150a6b4e2f
Mikhail_A <artemev1986@yandex.ru>
2023-09-13 00:48:53 +0500

    feat: modify test

--- src/test/java/com/example/calculatingfrequency/FrequencyControllerTest.java
+++ src/test/java/com/example/calculatingfrequency/FrequencyControllerTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -17,21 +19,18 @@ public class FrequencyControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    public void testCalculateFrequencyWithMixedInput() throws Exception {
+    public void testCalculateFrequency() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/calculateFrequency")
-                        .param("inputString", "abcABC1!23!!!@#"))
+                        .param("inputString", "aaaaabcccc!@#$"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.a").value(1))
+                .andExpect(jsonPath("$.*", hasSize(7)))
+                .andExpect(jsonPath("$.a").value(5))
+                .andExpect(jsonPath("$.c").value(4))
                 .andExpect(jsonPath("$.b").value(1))
-                .andExpect(jsonPath("$.c").value(1))
-                .andExpect(jsonPath("$.A").value(1))
-                .andExpect(jsonPath("$.B").value(1))
-                .andExpect(jsonPath("$.C").value(1))
-                .andExpect(jsonPath("$.1").value(1))
-                .andExpect(jsonPath("$.2").value(1))
-                .andExpect(jsonPath("$.3").value(1))
-                .andExpect(jsonPath("$.!").value(4))
+                .andExpect(jsonPath("$.!").value(1))
                 .andExpect(jsonPath("$.@").value(1))
-                .andExpect(jsonPath("$.#").value(1));
+                .andExpect(jsonPath("$.#").value(1))
+                .andExpect(jsonPath("$.$").value(1))
+                .andExpect(jsonPath("$.*", contains(5, 4, 1, 1, 1, 1, 1)));
     }
 }
